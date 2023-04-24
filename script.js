67c83b0a8e1dd48f00b9bfe25e7efd01128d067f
Vladyslav <131457331+vladyslavkoval03@users.noreply.github.com>
2023-04-25 01:50:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,153 @@
-function generateStars() {
-   const starsContainer = document.querySelector('.stars-container');
-   const numStars = 100;
-   const starSize = 2;
-   const starColor = 'white';
-   const lineColor = 'rgba(255, 255, 255, 0.5)';
-   const stars = [];
-   for (let i = 0; i < numStars; i++) {
-      const star = document.createElement('div');
-      star.classList.add('star');
-      star.style.width = `${starSize}px`;
-      star.style.height = `${starSize}px`;
-      star.style.backgroundColor = starColor;
-      stars.push(star);
-      starsContainer.appendChild(star);
-   }
-   for (let i = 0; i < numStars; i++) {
-      for (let j = i + 1; j < numStars; j++) {
-         const line = document.createElement('div');
-         line.classList.add('line');
-         line.style.backgroundColor = lineColor;
-         starsContainer.appendChild(line);
-         animateLine(line, stars[i], stars[j]);
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 90,
+      "density": {
+        "enable": true,
+        "value_area": 800
+          
       }
-   }
+      
+   
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 2,
+        "color": "#fff"
+      },
+      "polygon": {
+        "nb_sides": 7
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 3,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 20,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 250,
+      "color": "fff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+      
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+        
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 300,
+        "size": 70,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 500,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+ 
+});
+
+// JavaScript-код
+const target = document.querySelector('.animation-target');
+const words = ['JavaScript', 'CSS', 'HTML'];
+
+let wordIndex = 0;
+let letterIndex = 0;
+
+function type() {
+  if (letterIndex < words[wordIndex].length) {
+    // Виводимо на екран літеру за літерою
+    const span = document.createElement('span');
+    span.textContent = words[wordIndex].charAt(letterIndex);
+    target.appendChild(span);
+    letterIndex++;
+    setTimeout(type, 100);
+  } else {
+    // Якщо слово написане, то затримуємо його на екрані протягом 1 секунди
+    setTimeout(erase, 1000);
+  }
 }
 
-function animateLine(line, star1, star2) {
-   const lineLength = getDistance(star1, star2);
-   const angle = getAngle(star1, star2);
-   line.style.width = `${lineLength}px`;
-   line.style.transform = `rotate(${angle}rad) translate(-50%, -50%)`;
-   line.style.top = `${(star1.offsetTop + star2.offsetTop) / 2}px`;
-   line.style.left = `${(star1.offsetLeft + star2.offsetLeft) / 2}px`;
-   line.style.opacity = '0';
-   setTimeout(() => {
-      line.style.opacity = '1';
-   }, 1000);
-   setTimeout(() => {
-      animateLine(line, star1, star2);
-   }, 5000);
+function erase() {
+  if (letterIndex > 0) {
+    // Стираємо літеру за літерою
+    const span = target.querySelector('span:last-child');
+    span.parentNode.removeChild(span);
+    letterIndex--;
+    setTimeout(erase, 50);
+  } else {
+    // Якщо слово стерто, то переходимо до наступного слова
+    wordIndex++;
+    if (wordIndex >= words.length) {
+      wordIndex = 0;
+    }
+    setTimeout(type, 500);
+  }
 }
 
-function getDistance(star1, star2) {
-   const x1 = star1.offsetLeft;
-   const y1 = star1.offsetTop;
-   const x2 = star2.offsetLeft;
-   const y2 = star2.offsetTop;
-   const xDiff = x2 - x1;
-   const yDiff = y2 - y1;
-   return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
-}
-
-function getAngle(star1, star2) {
-   const x1 = star1.offsetLeft;
-   const y1 = star1.offsetTop;
-   const x2 = star2.offsetLeft;
-   const y2 = star2.offsetTop;
-   const xDiff = x2 - x1;
-   const yDiff = y2 - y1;
-   return Math.atan2(yDiff, xDiff);
-}
+type(); // Починаємо анімацію з першим словом
